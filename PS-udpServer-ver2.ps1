ed88665e25da87419e5a05f8813a7f3d664a23e6
Anas Hamra <anas.hamra@gmail.com>
2022-08-13 02:44:54 -0400

    Create PS-udpServer-ver2.ps1

---
+++ PS-udpServer-ver2.ps1
@@ -0,0 +1,14 @@
+# UDP Server version 2
+ 
+  $port = 2025
+  $endpoint = New-Object System.Net.IPEndPoint ([IPAddress]::Any, $port)
+  Try {
+      while($true) {
+          $socket = New-Object System.Net.Sockets.UdpClient $port
+          $content = $socket.Receive([ref]$endpoint)
+          $socket.Close()
+          [Text.Encoding]::ASCII.GetString($content)
+      }
+  } Catch {
+      "$($Error[0])"
+  } 
